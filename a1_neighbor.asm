af484b8c986a4f24c7b853fd852b0fd405a386f9
Simon Kunz <simon.kunz21@gmail.com>
2021-12-29 03:36:37 +0100

    finalize implementation

--- a1_neighbor.asm
+++ a1_neighbor.asm
@@ -69,54 +69,51 @@ test_neighbor_loop_direction:
 	#+ -----------------
 
 neighbor:
-	jr $ra
-	# decode packed position into x ($s0) and y ($s1) cordinates
+	# decode packed position into x ($t0) and y ($t1) cordinates
+
+	# check for bogus position
+	bgt $a0, 63, err_exit
 	
-	# and with 0b0000_01111 bitmask to extract the last 3 bits of the position
-	andi $s0, $a0, 7
+	# and with 0b0000_0111 bitmask to extract the last 3 bits of the position
+	andi $t0, $a0, 7
 
 	# shift 3 bits to the right to get the y cordinate from the upper 3 bits
-	srl $s1, $a0, 3
+	srl $t1, $a0, 3
+	
 
 	# jump to case for direction
-	#beq $a1, 0, case_direction_up
-	#beq $a1, 1, case_direction_down
-	#beq $a1, 2, case_direction_left
-	#beq $a1, 3, case_direction_right
-	#bgt $a1, 3, case_direction_err
-	
-	# should be unreachable
-
-calc_index_and_return:
-	# index = y * 9 + x
-	mul $t1, $s1, 9
-	addu $v0, $t1, $s0
-	jr $ra
+	beq $a1, 0, case_direction_up
+	beq $a1, 1, case_direction_left
+	beq $a1, 2, case_direction_down
+	beq $a1, 3, case_direction_right
+	bgt $a1, 3, case_direction_err
 
 case_direction_up:
 	# if y == 0 
-	beq $s1, 0, err_exit,
-	addi $s1, $s1, -1
+	beq $t1, 0, err_exit,
+	li $t3, 1
+	sub $t1, $t1, $t3
 	j calc_index_and_return
 
 case_direction_down:
 	# if y > 7
-	bge $s1, 7, err_exit
-	addi $s1, $s1, 1
+	bge $t1, 7, err_exit
+	addi $t1, $t1, 1
 	j calc_index_and_return
 
 case_direction_left:
 	# if x == 0
-	beq $s0, 0, err_exit
+	beq $t0, 0, err_exit
 	
-	addi $s0, $s0, -1
+	li $t3, 1
+	sub $t0, $t0, $t3
 	j calc_index_and_return
 		
 case_direction_right:
-	# if x > 7	
-	bgt $s0, 7, err_exit
+	# if x >= 7	
+	bge $t0, 7, err_exit
 
-	addi $s0, $s0, 1
+	addi $t0, $t0, 1
 	j calc_index_and_return
 
 case_direction_err: 
@@ -124,4 +121,10 @@ case_direction_err:
 
 err_exit:
 	li $v0, -1 
+	jr $ra
+
+calc_index_and_return:
+	# index = y * 8 + x
+	mul $t2, $t1, 8
+	addu $v0, $t2, $t0
 	jr $ra
